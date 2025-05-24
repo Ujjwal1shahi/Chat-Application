d856184b57174b084fc32af5cea9b7eeb4643591
Ujjwalsh1 <152307729+Ujjwalsh1@users.noreply.github.com>
2025-05-24 20:08:18 +0530

    Updated the contacts

--- Chat/src/App.jsx
+++ Chat/src/App.jsx
@@ -41,7 +41,6 @@ function App() {
         }else{
           setUserInfo(undefined);
         }
-        console.log({response});
       }catch(error){
         setUserInfo(undefined);
       }finally{
--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import cors from 'cors'
 import cookieParser from 'cookie-parser'
 import mongoose from 'mongoose'
 import authRoutes from './routes/AuthRoutes.js'
+import contactRoutes from './routes/ContactRoutes.js'
 
 dotenv.config();
 const app = express();
@@ -23,6 +24,7 @@ app.use(cookieParser());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/contacts", contactRoutes)
 
 const server = app.listen(port,() => {
      console.log(`Sever is running at http://localhost:${port}`);
